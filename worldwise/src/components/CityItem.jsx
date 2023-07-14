fc678a8892937676b34defa940ebde8f7ec85f76
LiZhe <1756368629@qq.com>
2023-07-13 21:01:31 -0400

    Dynamic Routes With URL Parameters

--- worldwise/src/components/CityItem.jsx
+++ worldwise/src/components/CityItem.jsx
@@ -1,20 +1,24 @@
 import styles from "./CityItem.module.css";
+import { Link } from "react-router-dom";
 
 function formatReadableDate(dateString) {
   return new Intl.DateTimeFormat("en", { day: "numeric", month: "long", year: "numeric" }).format(new Date(dateString));
 }
 
 function CityItem({ city }) {
-  const { cityName, emoji, date } = city;
+  const { cityName, emoji, date, id, position } = city;
+  console.log(position);
 
   return (
-    <li className={styles.cityItem}>
-      <span className={styles.emoji}>
-        <img src={`https://flagsapi.com/${emoji}/flat/24.png`} alt={cityName} />
-      </span>
-      <h3 className={styles.name}>{cityName}</h3>
-      <time className={styles.dates}>({formatReadableDate(date)})</time>
-      <button className={styles.deleteBtn}>&times;</button>
+    <li>
+      <Link className={styles.cityItem} to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
+        <span className={styles.emoji}>
+          <img src={`https://flagsapi.com/${emoji}/flat/24.png`} alt={cityName} />
+        </span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.dates}>({formatReadableDate(date)})</time>
+        <button className={styles.deleteBtn}>&times;</button>
+      </Link>
     </li>
   );
 }
