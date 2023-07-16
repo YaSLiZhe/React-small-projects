58082e2b4854d16135f14b5b5da7e67dc19c92b5
LiZhe <1756368629@qq.com>
2023-07-16 18:36:19 -0400

    Interacting With the Map

--- worldwise/src/components/Map.jsx
+++ worldwise/src/components/Map.jsx
@@ -1,17 +1,26 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import { useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer, useMap, useMapEvent } from "react-leaflet";
+import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesProvider";
 function Map() {
+  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
+  const mapLat = searchParams.get("lat");
+  const mapLng = searchParams.get("lng");
   const { cities } = useCities();
 
-  const [mapPosition, setMapposition] = useState([38.9108, -75.5277]);
+  const [mapPosition, setMapposition] = useState([40, 20]);
+  useEffect(
+    function () {
+      if (mapLat && mapLng) setMapposition([mapLat, mapLng]);
+    },
+    [mapLat, mapLng]
+  );
 
   return (
     <div className={styles.mapContainer}>
-      <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={true} className={styles.map}>
+      <MapContainer center={mapPosition} zoom={6} scrollWheelZoom={true} className={styles.map}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
@@ -26,9 +35,23 @@ function Map() {
             </Popup>
           </Marker>
         ))}
+        <ChangeMaker mapPosition={mapPosition} />
+        <DetectClick />
       </MapContainer>
     </div>
   );
+}
+
+function ChangeMaker({ mapPosition }) {
+  const map = useMap();
+  map.setView(mapPosition);
+  return null;
+}
+
+function DetectClick() {
+  const navigate = useNavigate();
+
+  useMapEvent({ click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`) });
 }
 
 export default Map;
